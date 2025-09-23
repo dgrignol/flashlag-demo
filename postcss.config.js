f57d776d7eea00e35fac924770ed5473e40f4bcc
Damiano Grignolio <damiano@Damianos-MacBook-Air-1028.local>
2025-09-23 12:24:35 +0200

    Make it pretty AKA Tailwind v4: use @tailwindcss/postcss and @import syntax

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// /Users/damiano/Documents/projects/flashlag-demo/postcss.config.js
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},   // ← use the v4 PostCSS plugin
+    autoprefixer: {},
+  },
+};
